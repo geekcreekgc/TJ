249a284657f4b229ddad83ece8ca05d66f77b2de
Tech Junction <you@example.com>
2021-11-20 15:48:00 +0530

    2.8V

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-var Info=("© 2020-2021 TechJunction.online | 2.3v");
-console.log(Info);
+var Info=("© 2020-2021 TechJunction.online | 2.6v |");
+var credit =(" The Website created with Bootstarp and Tailwind CSS, Developers : Ayush Baral , Sariul Hisab Arnav");
+console.log(Info + credit);
+
+
+var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+var alertTrigger = document.getElementById('liveAlertBtn')
+
+function alert(message, type) {
+  var wrapper = document.createElement('div')
+  wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+
+  alertPlaceholder.append(wrapper)
+}
+
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', function () {
+    alert('Nice, you triggered this alert message!', 'success')
+  })
+}
