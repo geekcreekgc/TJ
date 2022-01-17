1381f9441f992e06c7240ef6face4162add10925
Ayush <socialmedia.bb.yt@gmail.com>
2022-01-17 13:02:21 +0530

    Version 4.0

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-var Info=("© 2020-2021 TechJunction.online | 2.6v |");
-var credit =(" The Website created with Bootstarp and Tailwind CSS, Developers : Ayush Baral , Sariul Hisab Arnav");
-console.log(Info + credit);
-
-
 var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
 var alertTrigger = document.getElementById('liveAlertBtn')
 
 function alert(message, type) {
   var wrapper = document.createElement('div')
   wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
-
+  
   alertPlaceholder.append(wrapper)
 }
 
@@ -18,3 +13,7 @@ if (alertTrigger) {
     alert('Nice, you triggered this alert message!', 'success')
   })
 }
+
+var Info=("© 2020-2021 TechJunction.online | 4.0v |");
+var credit =(" The Website created with Bootstarp and Tailwind CSS, Developers : Ayush Baral , Saptak Dey");
+console.log(Info + credit);
