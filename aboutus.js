eed7a3f2ff9411d805b8e68c058bcba56be1395d
Ayush Baral <socialmedia.bb.yt@gmail.com>
2022-03-29 11:06:55 +0530

    change the about section

---
+++ aboutus.js
@@ -0,0 +1,222 @@
+function official() {
+  document.getElementById("card").innerHTML = `
+  
+  <!-- Ayush Baral -->
+
+    <div class="flex flex-col items-center">
+      
+      <img class="object-cover w-full rounded-xl aspect-square" src="/img/member/Ayush.jpeg" alt="Ayush Baral">
+       <h1 class="mt-4 text-2xl font-semibold text-yellow-500 capitalize">Ayush Baral</h1>
+        <p class="mt-2 capitalize text-white">Founder</p>
+    
+      <div class="flex mt-3 -mx-2">
+        
+        <a href="https://twitter.com/mr_2by" class="mx-2 text-gray-500 hover:text-yellow-100" aria-label="Twitter">    
+            <svg class="w-6 h-6 " ill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              class="w-5 h-5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/></svg>
+        </a>
+
+        <a href="https://www.instagram.com/mr_2by/" class="mx-2 text-gray-500 hover:text-yellow-100" aria-label="Instagram">
+            <svg class="w-6 h-6 " ill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+             class="w-5 h-5" viewBox="0 0 24 24">
+              <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                 <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+            </svg>
+        </a>
+
+        <a href="https://www.instagram.com/mr_2by/" class="mx-2 text-gray-500 hover:text-yellow-100" aria-label="Facebook">
+            <svg class="w-6 h-6 " fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
+              viewBox="0 0 24 24">
+              <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+            </svg>
+        </a>
+
+       </div>
+    </div>
+
+
+    <!-- Ankan Mallick -->
+
+    <div class="flex flex-col items-center">
+      
+      <img class="object-cover w-full rounded-xl aspect-square" src="/img/member/Ankan Mallick.jpg" alt="Ankan Mallick">
+       <h1 class="mt-4 text-2xl font-semibold text-yellow-500 capitalize">Ankan Mallick</h1>
+        <p class="mt-2 capitalize text-white">Content Writer</p>
+    
+      <div class="flex mt-3 -mx-2">
+
+        <a href="https://www.instagram.com/ankan.mallick.2210/" class="mx-2 text-gray-500 hover:text-yellow-100" aria-label="Instagram">
+            <svg class="w-6 h-6 " ill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+             class="w-5 h-5" viewBox="0 0 24 24">
+              <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                 <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+            </svg>
+        </a>
+
+        <a href="https://www.facebook.com/ankan.mallick.775" class="mx-2 text-gray-500 hover:text-yellow-100" aria-label="Facebook">
+            <svg class="w-6 h-6 " fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
+              viewBox="0 0 24 24">
+              <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+            </svg>
+        </a>
+
+       </div>
+    </div>
+
+    <!-- Saptak Dey -->
+
+    <div class="flex flex-col items-center">
+      
+      <img class="object-cover w-full h-full rounded-xl aspect-square" src="img/member/Saptak_Dey.jpeg" alt="Saptak Dey">
+       <h1 class="mt-4 text-2xl font-semibold text-yellow-500 capitalize">Saptak Dey</h1>
+        <p class="mt-2 capitalize text-white">Developer</p>
+    
+      <div class="flex mt-3 -mx-2">        
+
+        <a href="https://www.instagram.com/tj_noxx/" class="mx-2 text-gray-500 hover:text-yellow-100" aria-label="Instagram">
+            <svg class="w-6 h-6 " ill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+             class="w-5 h-5" viewBox="0 0 24 24">
+              <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                 <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+            </svg>
+        </a>
+
+        <a href="https://www.facebook.com/profile.php?id=100077469756925" class="mx-2 text-gray-500 hover:text-yellow-100" aria-label="Facebook">
+            <svg class="w-6 h-6 " fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
+              viewBox="0 0 24 24">
+              <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+            </svg>
+        </a>
+
+       </div>
+    </div>
+
+    <!-- Debrup Sen -->
+
+    <div class="flex flex-col items-center">
+      
+      <img class="object-cover w-full rounded-xl aspect-square" src="/img/member/DebrupSen.jpeg" alt="Debrup Sen">
+       <h1 class="mt-4 text-2xl font-semibold text-yellow-500 capitalize">Debrup Sen</h1>
+        <p class="mt-2 capitalize text-white">Editor</p>
+    
+      <div class="flex mt-3 -mx-2">
+
+        <a href="https://www.instagram.com/srvdeb59/" class="mx-2 text-gray-500 hover:text-yellow-100" aria-label="Instagram">
+            <svg class="w-6 h-6 " ill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+             class="w-5 h-5" viewBox="0 0 24 24">
+              <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                 <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+            </svg>
+        </a>
+
+
+       </div>
+    </div>
+
+    `;
+}
+
+function intern() {
+  document.getElementById("card").innerHTML = `
+    
+  <!--Anusmita Shome-->
+
+    <div class="flex flex-col items-center">
+                        <img class="object-cover w-full rounded-xl aspect-square" src="/img/member/Anusmita%20Shome.jpg" alt="Anusmita Shome">
+                        
+                        <h1 class="mt-4 text-2xl font-semibold text-yellow-500 capitalize">Anusmita Shome</h1>
+                        
+                        <p class="mt-2 capitalize text-white">Editor</p>
+
+                    </div>
+
+                    <!-- Samrat Sadhukhan -->
+
+                    <div class="flex flex-col items-center">
+                      
+                      <img class="object-cover h-full w-full rounded-xl aspect-square" src="/img/member/SamratSadhukhan.jpg" alt="Debrup Sen">
+                       <h1 class="mt-4 text-2xl font-semibold text-yellow-500 capitalize">Samrat Sadhukhan</h1>
+                        <p class="mt-2 capitalize text-white">Developer</p>
+                    
+                      <div class="flex mt-3 -mx-2">
+                
+                        <a href="https://www.instagram.com/sam._.iitd/" class="mx-2 text-gray-500 hover:text-yellow-100" aria-label="Instagram">
+                            <svg class="w-6 h-6 " ill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                             class="w-5 h-5" viewBox="0 0 24 24">
+                              <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                                 <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                            </svg>
+                        </a>
+                
+                
+                       </div>
+                    </div>
+
+                    <!-- Meghna Roy -->
+
+                    <div class="flex flex-col items-center">
+                      
+                      <img class="object-cover w-full rounded-xl aspect-square" src="/img/member/Meghna%20Roy.jpeg" alt="Meghna Roy">
+                       <h1 class="mt-4 text-2xl font-semibold text-yellow-500 capitalize">Meghna Roy</h1>
+                        <p class="mt-2 capitalize text-white">Blog Writer</p>
+                    
+                      <div class="flex mt-3 -mx-2">
+                
+                        <a href="https://www.instagram.com/meghnaaaaa_._" class="mx-2 text-gray-500 hover:text-yellow-100" aria-label="Instagram">
+                            <svg class="w-6 h-6 " ill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                             class="w-5 h-5" viewBox="0 0 24 24">
+                              <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                                 <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                            </svg>
+                        </a>
+                
+                
+                       </div>
+                    </div>
+                    
+                    <!-- Agniva Paul -->
+
+                    <div class="flex flex-col items-center">
+                      
+                      <img class="object-cover w-full rounded-xl aspect-square" src="/img/member/Agniva%20Paul.jpeg" alt="Agniva Paul">
+                       <h1 class="mt-4 text-2xl font-semibold text-yellow-500 capitalize">Agniva Paul</h1>
+                        <p class="mt-2 capitalize text-white">Blog Writer</p>
+                    
+                      <div class="flex mt-3 -mx-2">
+                
+                        <a href="https://www.instagram.com/agniva_paul/" class="mx-2 text-gray-500 hover:text-yellow-100" aria-label="Instagram">
+                            <svg class="w-6 h-6 " ill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                             class="w-5 h-5" viewBox="0 0 24 24">
+                              <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                                 <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                            </svg>
+                        </a>
+                
+                
+                       </div>
+                    </div>
+
+                    <!-- Nilesh Roy Chowdhury -->
+
+                    <div class="flex flex-col items-center">
+                      
+                      <img class="object-cover w-full rounded-xl aspect-square" src="/img/member/Nilesh%20Roy%20Chowdhury.jpg" alt="Nilesh Roy Chowdhury">
+                       <h1 class="mt-4 text-2xl font-semibold text-yellow-500 capitalize">Nilesh Roy Chowdhury</h1>
+                        <p class="mt-2 capitalize text-white">Developer</p>
+                    
+                      <div class="flex mt-3 -mx-2">
+                
+                        <a href="https://www.instagram.com/agniva_paul/" class="mx-2 text-gray-500 hover:text-yellow-100" aria-label="Instagram">
+                            <svg class="w-6 h-6 " ill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                             class="w-5 h-5" viewBox="0 0 24 24">
+                              <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                                 <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                            </svg>
+                        </a>
+                
+                
+                       </div>
+                    </div>
+
+    `;
+}
