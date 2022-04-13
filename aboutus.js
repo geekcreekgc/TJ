83b6bfc46fb1382656c19ff494e2e2bfa4b7a3b1
Ayush Baral <75385781+mr-2bY@users.noreply.github.com>
2022-04-13 11:07:53 +0530

    Adding new feature

--- aboutus.js
+++ aboutus.js
@@ -11,12 +11,12 @@ function official() {
     
       <div class="flex mt-3 -mx-2">
         
-        <a href="https://twitter.com/mr_2by" class="mx-2 text-gray-500 hover:text-yellow-100" aria-label="Twitter">    
+        <a href="https://twitter.com/2bY_dev" class="mx-2 text-gray-500 hover:text-yellow-100" aria-label="Twitter">    
             <svg class="w-6 h-6 " ill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
               class="w-5 h-5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/></svg>
         </a>
 
-        <a href="https://www.instagram.com/mr_2by/" class="mx-2 text-gray-500 hover:text-yellow-100" aria-label="Instagram">
+        <a href="https://www.instagram.com/2by.dev/" class="mx-2 text-gray-500 hover:text-yellow-100" aria-label="Instagram">
             <svg class="w-6 h-6 " ill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
              class="w-5 h-5" viewBox="0 0 24 24">
               <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
@@ -24,7 +24,7 @@ function official() {
             </svg>
         </a>
 
-        <a href="https://www.instagram.com/mr_2by/" class="mx-2 text-gray-500 hover:text-yellow-100" aria-label="Facebook">
+        <a href="https://www.facebook.com/2bY.dev" class="mx-2 text-gray-500 hover:text-yellow-100" aria-label="Facebook">
             <svg class="w-6 h-6 " fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
               viewBox="0 0 24 24">
               <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
