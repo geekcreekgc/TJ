1f93ad1f173a9ea6ede2aaabf34c2ff51d8e7dc8
Ayush Baral <socialmedia.bb.yt@gmail.com>
2022-05-19 14:47:43 +0530

    7.0.1v

--- script.js
+++ script.js
@@ -14,6 +14,6 @@ if (alertTrigger) {
   })
 }
 
-var Info=("© 2020-2022 TechJunction.online | 4.2v |");
+var Info=("© 2020 TechJunction.online | 7.0.1v |");
 var credit =(" The Website created with Bootstarp and Tailwind CSS, Developers : Ayush Baral , Saptak Dey");
 console.log(Info + credit);
