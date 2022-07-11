bb9add6b1eec8c7f41821205668b21bc37916517
Ayush Baral <socialmedia.bb.yt@gmail.com>
2022-07-12 05:18:06 +0530

    Geek Gallery

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
-var alertTrigger = document.getElementById('liveAlertBtn')
+// var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+// var alertTrigger = document.getElementById('liveAlertBtn')
 
-function alert(message, type) {
-  var wrapper = document.createElement('div')
-  wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+// function alert(message, type) {
+//   var wrapper = document.createElement('div')
+//   wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
   
-  alertPlaceholder.append(wrapper)
-}
+//   alertPlaceholder.append(wrapper)
+// }
 
-if (alertTrigger) {
-  alertTrigger.addEventListener('click', function () {
-    alert('Nice, you triggered this alert message!', 'success')
-  })
-}
+// if (alertTrigger) {
+//   alertTrigger.addEventListener('click', function () {
+//     alert('Nice, you triggered this alert message!', 'success')
+//   })
+// }
 
 var Info=("© 2020-22 TechJunction.online | 7.1.5v |");
 var credit =(" The Website created with Bootstarp and Tailwind CSS, Developers : Ayush Baral , Saptak Dey");
